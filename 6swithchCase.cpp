2c51bd20d270753de4e20a1d6ce9e90190c37b08
navdeep111 <guptanavdeep9461@gmail.com>
2024-12-17 18:39:08 +0530

    first commit

---
+++ 6swithchCase.cpp
@@ -0,0 +1,49 @@
+// #include<iostream> 
+// using namespace std;
+
+// int main(){
+
+// 	int day = 2;
+
+// 	switch(day){
+// 	case 1: 
+// 		cout << "Sunday";
+// 	    break;
+
+// 	case 2: 
+// 		cout << "Monday";
+// 	    break;
+	    
+//     case 3: 
+// 		cout << "Tuesday";
+// 	    break;
+
+
+// 	}
+
+// 	return 0;
+// }
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    const int x = 10;
+    const int y = 5;
+
+    switch (x + y) {
+        case 15:
+            cout << "Result is 15." << endl;
+            break;
+        case 20:
+            cout << "Result is 20." << endl;
+            break;
+        default:
+            cout << "No match found." << endl;
+    }
+
+    return 0;
+}
